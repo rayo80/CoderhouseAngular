72afee8e50f42595a871f119d291237801678fd4
Sergio <sergiovilla242@gmail.com>
2022-05-08 23:27:15 -0500

    Primer Desafio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidemenuComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
